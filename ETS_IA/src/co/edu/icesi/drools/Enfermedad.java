af7c7c77d7d606488f6612eb2fbeeb766f11d074
mishale22 <sami_na11@hotmail.com>
2018-12-01 21:21:00 -0500

    estructuraProyecto

---
+++ ETS_IA/src/co/edu/icesi/drools/Enfermedad.java
@@ -0,0 +1,10 @@
+package co.edu.icesi.drools;
+
+public class Enfermedad {
+
+	private String nombrePersona;
+	
+	private String fiebre;
+	private String flujo;
+	private String sangrado;
+}
---
+++ ETS_IA/src/co/edu/icesi/drools/PruebaEnfermedad.java
@@ -0,0 +1,10 @@
+package co.edu.icesi.drools;
+
+public class PruebaEnfermedad {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
